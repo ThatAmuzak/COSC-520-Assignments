85178ea6e75620390319d7476a6c4217170a5fb7
ThatAmuzak <rishavbanerjee1710@gmail.com>
2025-10-03 18:22:21 -0700

    added tests for binary and hashmap searches

--- Assignment-1/tests/test_search_algorithms.py
+++ Assignment-1/tests/test_search_algorithms.py
@@ -1,5 +1,11 @@
 import pytest
-from src.search_algorithms import binary_search, linear_search
+from src.search_algorithms import (
+    BloomFilter,
+    binary_search,
+    generate_hash_table,
+    hash_search,
+    linear_search,
+)
 from typeguard import TypeCheckError
 
 
@@ -8,7 +14,6 @@ def test_linear_search():
     Tests the linear search function
     We only test for existence, since
     that's the login checker's goal.
-    -1 indicates the item was not found
     """
     assert linear_search(["aa"], "aa")  # should find
     assert not linear_search([], "aa")  # should not find
@@ -17,7 +22,9 @@ def test_linear_search():
     assert linear_search(["c", "b", "a"], "b")  # should find
     assert not linear_search(["a", "b", "c"], "d")  # should not find
     with pytest.raises(TypeCheckError):
-        linear_search(1, 2) == 1  # should raise type error
+        linear_search(1, "aa")  # should raise type error
+    with pytest.raises(TypeCheckError):
+        linear_search(["aa"], 1)  # should raise type error
 
 
 def test_binary_search():
@@ -25,8 +32,52 @@ def test_binary_search():
     Tests the binary search function
     We only test for existence, since
     that's the login checker's goal.
-    -1 indicates the item was not found
     """
     assert binary_search(["aa"], "aa")  # should find
     assert not binary_search([], "aa")  # should not find
+    assert not binary_search(["aa"], "")  # should not find
     assert binary_search(["a", "b", "c"], "b")  # should find
+    assert not binary_search(["a", "b", "c"], "d")  # should not find
+    assert binary_search(["a", "b", "c", "d"], "c")  # should find
+    with pytest.raises(TypeCheckError):
+        binary_search(1, "a")  # should raise type error
+    with pytest.raises(TypeCheckError):
+        binary_search(["a"], 1)  # should raise type error
+
+
+def test_hashmap_search():
+    """
+    Tests the hashmap search function
+    We only test for existence, since
+    that's the login checker's goal.
+    """
+    assert hash_search(generate_hash_table(["aa"]), "aa")  # should find
+    assert not hash_search(generate_hash_table([]), "aa")  # should not find
+    assert not hash_search(generate_hash_table(["aa"]), "")  # should not find
+    assert hash_search(generate_hash_table(["a", "b", "c"]), "b")  # should find
+    assert not hash_search(generate_hash_table(["a", "b", "c"]), "d")  # should not find
+    assert hash_search(generate_hash_table(["d", "c", "b", "a"]), "c")  # should find
+    with pytest.raises(TypeCheckError):
+        hash_search(generate_hash_table(1), "a")  # should raise type error
+    with pytest.raises(TypeCheckError):
+        hash_search(generate_hash_table(["a"]), 1)  # should raise type error
+
+
+def test_bloom_search():
+    """
+    Tests the binary search function
+    We only test for existence, since
+    that's the login checker's goal.
+    """
+    filter = BloomFilter()
+    filter.add("aa")
+    assert filter.get("aa")  # should find
+    # assert not hash_search(generate_hash_table([]), "aa")  # should not find
+    # assert not hash_search(generate_hash_table(["aa"]), "")  # should not find
+    # assert hash_search(generate_hash_table(["a", "b", "c"]), "b")  # should find
+    # assert not hash_search(generate_hash_table(["a", "b", "c"]), "d")  # should not find
+    # assert hash_search(generate_hash_table(["d", "c", "b", "a"]), "c")  # should find
+    # with pytest.raises(TypeCheckError):
+    #     hash_search(generate_hash_table(1), "a")  # should raise type error
+    # with pytest.raises(TypeCheckError):
+    #     hash_search(generate_hash_table(["a"]), 1)  # should raise type error
